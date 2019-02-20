d839f0a52957cdc14967b35d9ba268ed0234517f
CodedStingray <ray.kampmann@gmail.com>
2019-02-20 19:00:40 +0100

    Wrapper classes for ItemAxe and ItemPickaxe to have public constructors

---
+++ src/main/java/net/codedstingray/magicalloy/items/util/ItemAxePC.java
@@ -0,0 +1,10 @@
+package net.codedstingray.magicalloy.items.util;
+
+import net.minecraft.item.ItemAxe;
+
+public class ItemAxePC extends ItemAxe {
+
+    public ItemAxePC(ToolMaterial material, float damage, float speed) {
+        super(material, damage, speed);
+    }
+}
---
+++ src/main/java/net/codedstingray/magicalloy/items/util/ItemPickaxePC.java
@@ -0,0 +1,9 @@
+package net.codedstingray.magicalloy.items.util;
+
+import net.minecraft.item.ItemPickaxe;
+
+public class ItemPickaxePC extends ItemPickaxe {
+    public ItemPickaxePC(ToolMaterial material) {
+        super(material);
+    }
+}
